6aa99e3262095fb087b0fb9cdcfa3a4fd22d1426
saleel vt <saleelvt57@gmail.com>
2024-07-19 09:02:06 +0530

    basic covered and advanced started

--- Basic-Typescript/index.js
+++ Basic-Typescript/index.js
@@ -3,12 +3,11 @@ var usernumbers = 30000;
 var isthere = true;
 var skills = ["singer", "swimmer", "player"];
 var skillers = [32, 434, 43];
-
-
-// interface
-
-var userDetails = {
-  name: "saleel",
-  age: 21,
-  place: "thiruveegapura",
+var empty = [4, 4];
+var array;
+var obj = {
+    name: 'saleel',
+    age: 21,
+    place: 'valanchery'
 };
+
--- Basic-Typescript/index.ts
+++ Basic-Typescript/index.ts
@@ -3,26 +3,15 @@ var usernumbers: number = 3000_0;
 var isthere: boolean = true;
 var skills: string[] = ["singer", "swimmer", "player"];
 var skillers: number[] = [32, 434, 43];
-
-interface Details {
-  name: string;
-  age: number;
-  place: string;
-  // getName: () => void;
-}
-
-var userDetails: Details = {
-  name: "saleel",
-  age: 21,
-  place: "thiruveegapura",
-  // getName() {
-  //   return this.name;
-  // },
+var empty: number[] = [4, 4];
+var array: string | number;
+ var obj: { name: string; age: number; place: string } = {
+  name:'saleel',
+  age:21,
+  place:'valanchery'
 };
 
-// let newDetails:Details= {
 
-//   name:'gouse',
-//   age:22,
-//   place:'tirur'
-// }
+
+
+
